6426b80f212d2a651a8262a74d88ab59bb942e12
ira <rybakov.i.a.2002@gmail.com>
2024-03-08 19:28:26 +0300

    started

--- README.md
+++ README.md
@@ -1,15 +1,15 @@
-# benejor
+# Benejor
 
 * status - DEV
 
-## description
+## Description
 
 benejor is a simple telegrambot on python and mysql
 it saves users' passwords. this bot was made to help user in password management by generating and saving passwords.
 
 
 
-## instalation
+## Instalation
 1. `git clone https://github.com/onemorebruh/benejor.git`
 2. create database and user
 3. give privileges to this user
@@ -17,7 +17,7 @@ it saves users' passwords. this bot was made to help user in password management
 3. enter all data script asks
 4. run the controller.py
 
-## functions
+## Functions
 
 ```python
 from functions import charge, generate_password, encrypt, decrypt
@@ -32,10 +32,20 @@ print(encrypt("sailBUILDMEAThundredsizeHARDFOODyoung!BRANCH", 12345)) # somethin
 print(decrypt("116C99A108C112E71D86C75D79C72D82D70C67A87A108B122C111D102D117B105E105A116E107D125B105E77B66F84B71C74C84A80A70C124A115C122E111B105B36E70C87B66A80F70D76E", 12345))
 ```
 
-## configuration
+## Configuration
 - python:3.8.10
 - mysql:8.0.26
 - ubuntu:20.04
 
+## TODO
 
+Deploy
+- [ ] replace `get_started` script with Dockerfile and config file
 
+Rewrite on aiogram
+- [ ] write markups
+- [ ] implemets same algoritm as before
+- [ ] configure HTML parsing
+
+UX
+- [ ] add `copy password` button
---
+++ aiobot.py
@@ -0,0 +1,47 @@
+import asyncio
+from dotenv import load_dotenv
+import os
+
+from aiogram import Bot, Dispatcher, Router, types
+from aiogram.enums import ParseMode
+from aiogram.filters import CommandStart
+from aiogram.types import Message
+from aiogram.utils.markdown import hbold
+
+load_dotenv()
+BOT_TOKEN = os.getenv("BOT_TOKEN")
+OWNER_NAME = os.getenv("OWNER_NAME")
+
+# All handlers should be attached to the Router (or Dispatcher)
+dp = Dispatcher()
+
+
+@dp.message(CommandStart())
+async def command_start_handler(message: Message) -> None:
+    await message.answer(f"Welcome.\nI am bnejor. The bot who saves your passwords and generates safe passwords.\nYour passwords would be saved on {OWNER_NAME}\'s machine.\nHave a nice experience")
+
+
+@dp.message()
+async def echo_handler(message: types.Message) -> None:
+    """
+    Handler will forward receive a message back to the sender
+
+    By default, message handler will handle all message types (like a text, photo, sticker etc.)
+    """
+    try:
+        # Send a copy of the received message
+        await message.send_copy(chat_id=message.chat.id)
+    except TypeError:
+        # But not all the types is supported to be copied so need to handle it
+        await message.answer("Nice try!")
+
+
+async def main() -> None:
+    # Initialize Bot instance with a default parse mode which will be passed to all API calls
+    bot = Bot(BOT_TOKEN)
+    # And the run events dispatching
+    await dp.start_polling(bot)
+
+
+if __name__ == "__main__":
+    asyncio.run(main())
